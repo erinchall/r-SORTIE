89d11d66ed3a2aab2d96718a9950a85631f75df2
Erin Hall <ehall@unbc.ca>
2021-06-02 00:20:57 -0700

    Makes all parameter files and moves to own folder

--- MakeFiles_ss.R
+++ MakeFiles_ss.R
@@ -24,6 +24,7 @@ xml_path <- file.path("BaseParameterFiles")
 param_path <- file.path("ParameterValues")
 out_path <- file.path("output")
 list_path <- file.path("FileLists")
+sortie_path <- file.path("OutputSortieParFiles")
 
 #VARIABLES THAT DECIDE WHAT ELSE RUNS
 bRunSortie <- 0     #0= do not run Sortie, 1= Run Sortie with newly made xml files
@@ -33,10 +34,14 @@ YearsToExtract <- c("1","5","10")  #example PUT YOUR YEARS HERE for which tree y
 sortie_loc = 0    #location of the Sortie executable. If 0, it is in the default location. (C:\\Program Files (x86)\\SORTIE\\bin\\coremodel.exe)
 #otherwise, put a string with the location here. Use / instead of \
 
+###############################################################
+# STEP 1: Create parameter files with each harvest pattern
+###############################################################
+
 #Start processing
 #read the list of files         NOTE: number of allowable file types = 5
-for(in_n in 1:2){ #Add Loop to apply different MakeFiles and .xml chunks depending whether mpbn or mpby
-  lstFiles <- read.csv(file.path(list_path, paste0("files_ss", in_n, ".txt")))
+for(ss_n in 1:2){ #Add Loop to apply different MakeFiles and .xml chunks depending whether mpbn or mpby
+  lstFiles <- read.csv(file.path(list_path, paste0("files_ss", ss_n, ".txt")))
 
 xmlList <- c()
 paramList1 <- vector("list",5)
@@ -92,7 +97,7 @@ for (ix in 1:length(xmlList)) { #start loop over xml files
             ip_vals <- c(ip,ip2,ip3,ip4,ip5)
             newname <- ""
             #newname <- paste(substr(xmlList[ix],1,nchar(xmlList[ix])-4),"-",substr(paramList1[[1]][ip],1,nchar(paramList1[[1]][ip])-4),sep="")
-            newname <- paste(substr(xmlList[ix],1,2), substr(paramList1[[1]][ip],1,2), substr(xmlList[ix],9,16), sep = "_") # Change the output file name to insert the harvest type only
+            newname <- paste(substr(xmlList[ix],1,2), substr(paramList1[[1]][ip],1,2), substr(xmlList[ix],9,12), substr(xmlList[ix],16,16), sep = "_") # Change the output file name to insert the harvest type only
             #newname <- paste(substr(xmlList[ix],1,nchar(xmlList[ix])-4),sep="")    #remove type 1 files from the naming
             
             #for each of the files, prepare it, and process it
@@ -119,9 +124,124 @@ for (ix in 1:length(xmlList)) { #start loop over xml files
         }
       }
     }
-  }
+  } 
+} 
 }
-} # close MakeFiles loop for creating mpbn vs mpby parameter files (mpby has harvest and plant in positions 2 and 23 instead of 1 and 22 for mpbn base files)
+ 
+# close MakeFiles loop for creating mpbn vs mpby parameter files (mpby has harvest and plant in positions 2 and 23 instead of 1 and 22 for mpbn base files)
+
+###############################################################
+# STEP 2: Apply appropriate species mix to each parameter file
+###############################################################
+
+#Start processing
+#read the list of files         NOTE: number of allowable file types = 5
+
+for(in_n in 1:5){ #Add Loop to apply different MakeFiles and .xml chunks depending the initialization/stand type
+  lstFiles <- read.csv(file.path(list_path, paste0("files_ss_in", in_n, ".txt")))
+  
+  xmlList <- c()
+  paramList1 <- vector("list",5)
+  maxtype <- 0
+  for (i in 1:nrow(lstFiles)) {
+    fn <- as.character(trimws(lstFiles$name[i]))
+    itype <- lstFiles$type[i]
+    
+    #if (itype == 0) {
+      xmlList <- list.files(pattern = paste0("_", in_n, ".xml"))
+    #}
+    #else {
+      paramList1[[itype]] <- c(paramList1[[itype]],list(fn))
+    #}
+    if (itype > maxtype) {maxtype <- itype}
+  }
+  numtype <- c()
+  for (iii in 1:5) {
+    numtype <- c(numtype,length(paramList1[[iii]]))
+  }
+  
+  #Valid values of itype:
+  #           1=basic case: variable parameter is directly after the name
+  #           2=basic case with species: same, but with a species name after it
+  #           3=behaviorlist type: but basic parameter (like 1)
+  #           4=behaviorlist type: but with species (like 2)
+  #           5=output files: so parameter file will have a directory name
+  #           6=groups with species on the previous line (e.g. for initial density)
+  #col 1: input parameter (the name in Alana's file)
+  #col 2: type
+  #col 3: name in the line being replaced
+  #col 4: group name 
+  VariableNames <- read.csv(file.path(source_path, "VariableNames_pd.csv"), header=TRUE, strip.white = TRUE) # _pd refers to updated with plant density variable reference
+  
+  ListOfParFiles <- c()
+  for (ix in 1:length(xmlList)) { #start loop over xml files
+    
+    #read the given xml
+    res <- read_xml(file.path(xmlList[ix]))
+    #write the xml to a file again (this will put in the missing line breaks)
+    write_xml(res, "temp.xml")
+    
+    #read the newly printed file, this time as lines of text
+    tmp <- readLines("temp.xml", encoding="UTF-8")
+    xml1 <- gsub("\\\\", "//",tmp)    #reverse the slash marks
+    
+    #make a vector that contains the length of each file type
+    for (ip in 1:numtype[1]) {
+      for (ip2 in 1:max(1,numtype[2])) {
+        for (ip3 in 1:max(1,numtype[3])) {
+          for (ip4 in 1:max(1,numtype[4])) {
+            for (ip5 in 1:max(1,numtype[5])) {
+              ip_vals <- c(ip,ip2,ip3,ip4,ip5)
+              newname <- ""
+              #newname <- paste(substr(xmlList[ix],1,nchar(xmlList[ix])-4),"-",substr(paramList1[[1]][ip],1,nchar(paramList1[[1]][ip])-4),sep="")
+              newname <- paste0(substr(xmlList[ix],1,10), "_in", in_n) # Change the output file name to insert the harvest type only
+              #newname <- paste(substr(xmlList[ix],1,nchar(xmlList[ix])-4),sep="")    #remove type 1 files from the naming
+              
+              #for each of the files, prepare it, and process it
+              # note: we have to do all five files each time because we don't know which of the files might have the output directories (which need 'newname')
+              
+              for (iii in 1:5) {
+                if (numtype[iii] > 0) {
+                  #print(paste("MakeFiles",iii,ip_vals[iii]))
+                  #print(paramList1[[iii]][ip_vals[iii]])
+                  xml2 <- ModifyFile(file.path(param_path, paramList1[[iii]][ip_vals[iii]]),xml1)
+                } else {
+                  xml2 <- xml1
+                }
+                xml1 <- xml2
+              } 
+              
+              xml2 <- gsub("//", "\\\\", xml2)    #turn any forward slashes into back into double backwards slashes
+              #write the new file 
+              newname <- paste(newname,".xml",sep="")
+              #newname <- "erin/debug2.xml"
+              writeLines(xml2,newname)
+              ListOfFiles <- c(ListOfFiles, newname)    #store the newly created file in a list so it can be run automatically later
+            }
+          }
+        }
+      }
+    }  ListOfParFiles <- c(ListOfParFiles, newname)
+  }
+} # close MakeFiles loop for adding planting specs (species %) 
+
+
+###############################################
+#   Clean up the root directory
+# Move parameter files into SORTIE Parameter File directory
+###############################################
+
+# Remove mid-processing files
+listing <- list.files()  
+filesDelete <- listing[grep("_[0-9].xml", listing )]    # all preliminary files
+file.remove(filesDelete)
+
+# Move final Sortie Parameter Files to self-contained Directory
+files <- list.files(pattern="*_in*", recursive=FALSE)
+file.copy(from=files, to=sortie_path, 
+          overwrite = TRUE, recursive = FALSE, 
+          copy.mode = TRUE)
+file.remove(files)
 
 ###############################################
 #Run the model with these files
